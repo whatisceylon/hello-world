ea2cb527ffea3d8d0f8acb4bbd03969c7f3aaca0
whatisceylon <whatisceylon@163.com>
2016-03-27 23:24:17 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorkd.java
@@ -0,0 +1,13 @@
+package com.youmeek.demo;
+
+/**
+ * Created by whatisceylon on 2016/3/27.
+ */
+public class HelloWorkd {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+    }
+}
